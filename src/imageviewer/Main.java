669acf2a5a462cf60f8854a5a01dcf8abc9d79cc
vengador41 <vengador41@192.168.0.105>
2017-12-28 11:50:16 +0000

    ImageViewer final

---
+++ src/imageviewer/Main.java
@@ -0,0 +1,23 @@
+
+package imageviewer;
+
+import imageviewer.control.NextImageCommand;
+import imageviewer.control.PrevImageCommand;
+import imageviewer.persistence.ImageLoader;
+import imageviewer.persistence.files.FileImageLoader;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        ImageLoader imageLoader = new FileImageLoader("C:\\Users\\JuanmaV41\\Imágenes");
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.add(new NextImageCommand(mainFrame.getImageDisplay()));
+        mainFrame.add(new PrevImageCommand(mainFrame.getImageDisplay()));
+        mainFrame.getImageDisplay().display(imageLoader.load());
+        
+    }
+    
+}
---
+++ src/imageviewer/ui/ImageDisplay.java
@@ -0,0 +1,11 @@
+
+package imageviewer.ui;
+
+import imageviewer.model.Image;
+
+public interface ImageDisplay {
+    
+    void display(Image image);
+    Image currentImage();
+    
+}
